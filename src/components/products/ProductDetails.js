ddbe46a05ab1a95d51efc50e21915c84d5de4cc9
Ronald Lankford <rolankford@gmail.com>
2020-09-02 15:26:51 -0500

    wip

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 export default function ProductDetails(props) {
 
     const [product, setProduct] = useState({})
+    const [pageReload, setPageReload] = useState(false)
 
     const getProduct = () => {
         fetch(`http://localhost:8000/products/${props.productId}`, {
@@ -17,20 +18,59 @@ export default function ProductDetails(props) {
         .then(products => {
             setProduct(products)
         })
+        .then(() => setPageReload(!pageReload))
+    }
+
+    const updatingQuantity = () => {
+        fetch(`http://localhost:8000/products/${props.productId}`, {
+            "method": "GET",
+            "headers": {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
+            }
+        })
     }
 
     useEffect (() => {
         getProduct()
+        // updatingQuantity()
     }, [])
 
-
+    const addingToOrder = () => {
+        fetch(`http://localhost:8000/orders`, {
+            'method': 'GET',
+            'headers': {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${localStorage.getItem("bangazon_token")}`
+            }
+        }).then(res => res.json())
+        .then(order => {
+            console.log(order[0].id)
+            console.log(product.id)
+            fetch(`http://localhost:8000/orderproducts`, {
+                'method': 'POST',
+                'headers': {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${localStorage.getItem("bangazon_token")}`
+                },
+                'body': JSON.stringify({
+                    product_id: product.id,
+                    order_id: order[0].id
+                })
+            })
+        })
+    }
 
     return (
         <div>
             <h2>{product.title}</h2>
             <p>{product.description}</p>
             <p>{product.price}</p>
-            <button>Add To Order(in progress)</button>
+            <p>{product.quantity}</p>
+            <button onClick={addingToOrder}>Add To Order</button>
         </div>
     )
 }
