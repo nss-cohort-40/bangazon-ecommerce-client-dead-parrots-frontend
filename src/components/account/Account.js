3bb11c97d6251c0ca5277c9d383c5dab0bf02cb1
Davis Lindell <daviselindell@gmail.com>
2020-09-02 14:25:29 -0500

    wip

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import AccountEditForm from './AccountEditForm';
 
-export default function Account() {
+const Account = props => {
+
+  const [ customerDetails , setCustomerDetails ] = useState({ user: {} });
+  const [ formOpen, setFormOpen ] = useState( false )
+
+  const getCustomer = () => {
+    return fetch("http://127.0.0.1:8000/customers", {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "Authorization": `token ${localStorage.getItem('bangazon_token')}`
+      }
+    })
+      .then(response => response.json())
+      .then((customer) => {
+        setCustomerDetails(customer[0]);
+      })
+  };
+
+
+  useEffect(() => {
+    getCustomer()}, []);
+
     return (
         <div>
-
+            <h2>Account</h2>
+            <p>Username: {customerDetails.user.username}</p>
+            <p>First Name: {customerDetails.user.first_name}</p>
+            <p>Last Name: {customerDetails.user.last_name}</p>
+            <p>Address: {customerDetails.address}</p>
+            <p>Phone Number: {customerDetails.phone_number}</p>
+            <button onClick={() => setFormOpen(true)}>Edit Account</button>
+            { formOpen ? <AccountEditForm setFormOpen={ setFormOpen } getCustomer={ getCustomer }/> : '' } 
         </div>
     )
-}
+};
+
+export default Account;
--- src/components/account/AccountEditForm.js
+++ src/components/account/AccountEditForm.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-export default function AccountEditForm() {
-    return (
-        <div>
+const AccountEditForm = props => {
+  const {
+    buttonLabel,
+    className
+  } = props;
 
-        </div>
-    )
+  const [modal, setModal] = useState(true);
+
+  const toggle = () => {
+    props.setFormOpen(false);
+    setModal(!modal);
+  }
+
+  useEffect(() => {
+    getCustomer()}, []);
+
+  return (
+    <div>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Account Edit Form</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
 }
+
+export default AccountEditForm;
